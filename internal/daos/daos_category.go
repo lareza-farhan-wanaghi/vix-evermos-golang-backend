c92bb2e70ff58c06ef36915911a575b6a90cabae
larezafw <farhanlareza@gmail.com>
2023-04-25 16:43:51 +0700

    Removed Ignored files

--- internal/daos/daos_category.go
+++
@@ -1,8 +0,0 @@
-package daos
-
-import "gorm.io/gorm"
-
-type Category struct {
-	gorm.Model
-	NamaCategory string
-}
--- internal/daos/daos_foto_produk.go
+++
@@ -1,9 +0,0 @@
-package daos
-
-import "gorm.io/gorm"
-
-type FotoProduk struct {
-	gorm.Model
-	IdProduk uint
-	Url      string
-}
